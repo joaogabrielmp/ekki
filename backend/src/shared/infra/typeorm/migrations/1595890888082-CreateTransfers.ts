4d27fa3c217ff96a1f0b611cdc1fa7d11c041b55
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-27 20:09:51 -0300

    chore: created Transfers migration

---
+++ backend/src/shared/infra/typeorm/migrations/1595890888082-CreateTransfers.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransfers1595890888082
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transfers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'beneficiary_id',
+            type: 'uuid',
+          },
+          {
+            name: 'balance',
+            type: 'int2',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'UserBeneficiariesSend',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'UserBeneficiariesReceive',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['beneficiary_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transfers');
+  }
+}
