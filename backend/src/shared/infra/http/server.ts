9786a89957700a84298b504efe9bfc0afef572a3
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-27 15:27:32 -0300

    setup project

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -6,7 +6,7 @@ import express, { NextFunction, Request, Response } from 'express';
 
 // import '@shared/infra/typeorm';
 // import '@shared/container';
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 // import routes from '@shared/infra/http/routes';
 
 const app = express();
@@ -16,23 +16,23 @@ app.use(express.json());
 // app.use(routes);
 app.use(errors());
 
-// app.use(
-//   (err: Error, request: Request, response: Response, _next: NextFunction) => {
-//     if (err instanceof AppError) {
-//       return response.status(err.statusCode).json({
-//         status: 'error',
-//         message: err.message,
-//       });
-//     }
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
 
-//     console.error(err);
+    console.error(err);
 
-//     return response.status(500).json({
-//       status: 'error',
-//       message: 'Internal server error',
-//     });
-//   },
-// );
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
