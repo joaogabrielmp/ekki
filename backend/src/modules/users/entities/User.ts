d05262fa21ed90c66bd034a6f4ff3f625172944c
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-27 20:15:21 -0300

    feat: created User entity

---
+++ backend/src/modules/users/entities/User.ts
@@ -0,0 +1,33 @@
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  cellphone: string;
+
+  @CreateDateColumn()
+  @Exclude()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  @Exclude()
+  updated_at: Date;
+}
+
+export default User;
