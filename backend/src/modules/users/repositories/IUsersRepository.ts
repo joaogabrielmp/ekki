86d6a5776508343bb0e395d97398fc44cf1e0a81
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-29 21:31:01 -0300

    refactor: Create Account

--- backend/src/modules/users/repositories/IUsersRepository.ts
+++ backend/src/modules/users/repositories/IUsersRepository.ts
@@ -1,12 +1,9 @@
 import IUserDTO from '@modules/users/dtos/IUserDTO';
-import Account from '@modules/accounts/entities/Account';
 import User from '@modules/users/entities/User';
 
 export default interface IUsersRepository {
   create(data: IUserDTO): Promise<User>;
-  findByAccountNumber(account_number: string): Promise<Account | undefined>;
   findByCPF(cpf: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
-  generateAccountNumber(account_number: string): Promise<string>;
   // update(data?: IUserDTO): Promise<void>;
 }
