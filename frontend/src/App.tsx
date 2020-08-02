87fe9a63446537467d11b62abf7e4dc7f7d71a20
joaogabrielmp <joaogabrielma@gmail.com>
2020-08-02 15:19:21 -0300

    chore: initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// import GlobalStyle from './styles/global';
+// import Routes from './routes';
+
+const App: React.FC = () => <h1>Ekki</h1>;
+
+export default App;
