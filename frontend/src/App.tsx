1e17425c83a45fb97e62ce19649239f22f63a1f6
joaogabrielmp <joaogabrielma@gmail.com>
2020-08-02 15:22:54 -0300

    feat: added global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global';
 // import Routes from './routes';
 
-const App: React.FC = () => <h1>Ekki</h1>;
+const App: React.FC = () => (
+  <Router>
+    {/* <Routes /> */}
+
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
